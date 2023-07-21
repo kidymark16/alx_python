02095a40c27c4261c61a7d79e0157fa7e8f37c88
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-07-21 10:44:45 +0300

    Update 2-positive_or_negative.py

--- python-hello_world/2-positive_or_negative.py
+++ python-hello_world/2-positive_or_negative.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-    print("is positive\n".format(number))
+    print("{}is positive\n".format(number))
 elif number == 0:
     print("{} is zero\n".format(number))
 else:
