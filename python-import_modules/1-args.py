5bb11be327d962e774721862ac7c80dee7408e5d
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-07-24 11:31:56 +0300

    Update 1-args.py

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,16 +1,17 @@
 import sys
 
 if __name__ == "__main__":
-    # get the number of arguments
-    num_args = len(sys.argv) - 1  # the first argument is the script name
+    # get the command-line arguments
+    args = sys.argv[1:]
 
     # print the number of arguments
+    num_args = len(args)
     print("Number of argument{}: {}".format("s" if num_args != 1 else "", num_args), end="")
 
     # print the list of arguments if there are any
     if num_args > 0:
-        print(":", end="\n")
-        for i, arg in enumerate(sys.argv[1:], start=1):
+        print(":")
+        for i, arg in enumerate(args, start=1):
             print("{}: {}".format(i, arg))
     else:
         print(".")
