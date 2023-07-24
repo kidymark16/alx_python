e870316a860441c19780c5fd4c95d6ac57c90f72
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-07-24 11:14:01 +0300

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,5 +1,19 @@
+# importing the add function from add_0.py
 from add_0 import add
-a = 1
-b = 2
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+
+# defining the main function
+def main():
+    # assigning values to variables a and b
+    a = 1
+    b = 2
+
+    # calling the add function and storing the result in a variable
+    result = add(a, b)
+
+    # printing the result using string formatting
+    print("{} + {} = {}".format(a, b, result))
+
+# checking if this is the main module
+if __name__ == "__main__":
+    # calling the main function
+    main()
