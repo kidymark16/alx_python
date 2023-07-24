64d72df2c2e588e3cefba44c0d826d28083f189e
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-07-24 11:21:49 +0300

    Update 0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,13 @@
 # importing the add function from add_0.py
 from add_0 import add
 
-# assigning values to variables a and b
-a = 1
-b = 2
+if __name__ == "__main__":
+    # assigning values to variables a and b
+    a = 1
+    b = 2
 
-# calling the add function and storing the result in a variable
-result = add(a, b)
+    # calling the add function and storing the result in a variable
+    result = add(a, b)
 
-# printing the result using string formatting
-print("{} + {} = {}".format(a, b, result))
+    # printing the result using string formatting
+    print("{} + {} = {}".format(a, b, result))
