8638e325682cfe25a94d0159f6463988cef66c1d
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-07-24 11:20:50 +0300

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,18 +1,12 @@
-def perform_addition():
-    # import the add function from add_0.py
-    from add_0 import add
+# importing the add function from add_0.py
+from add_0 import add
 
-    # assign values to variables a and b
-    a = 1
-    b = 2
+# assigning values to variables a and b
+a = 1
+b = 2
 
-    # call the add function and store the result in a variable
-    result = add(a, b)
+# calling the add function and storing the result in a variable
+result = add(a, b)
 
-    # print the result using string formatting
-    print("{} + {} = {}".format(a, b, result))
-
-# check if this is the main module
-if __name__ == "__main__":
-    # call the perform_addition function
-    perform_addition()
+# printing the result using string formatting
+print("{} + {} = {}".format(a, b, result))
