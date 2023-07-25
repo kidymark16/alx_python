1bc221f76564e24a179b5dbed9787e63f42007c6
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-07-25 12:09:19 +0300

    Update 1-print_matrix_integer.py

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,13 +1,3 @@
-def print_matrix_integer(matrix=None):
-    if matrix is None:
-        return
-
+def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in range(len(row)):
-            # Use str.format() to print each integer with a width of 2 characters
-            print("{:d}".format(row[i]), end="")
-            if i != len(row) - 1:
-                # Print a space after each integer except the last one
-                print(" ", end="")
-        # Print a newline character after each row
-        print()
+        print(" ".join("{:d}".format(num) for num in row))
