3d3c3dba2798d00cf69fc93a2c6d5c3e6cd3ff18
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-07-25 11:42:19 +0300

    0-square_matrix_simple.py

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,9 @@
-
-
 #!/usr/bin/python3
+"""
+This module contains a function that computes the square value of all integers of a matrix.
+"""
+
+
 def square_matrix_simple(matrix=[]):
     # Create a new matrix with the same size as the input matrix
     result = [[0 for j in range(len(matrix[i]))] for i in range(len(matrix))]
