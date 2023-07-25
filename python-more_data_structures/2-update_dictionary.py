b3f38c8920682e79734892d9415c60046e47c82a
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-07-25 11:43:59 +0300

    2-update_dictionary.py

---
+++ python-more_data_structures/2-update_dictionary.py
@@ -0,0 +1,9 @@
+def update_dictionary(a_dictionary, key, value):
+    # If the key exists in the dictionary, update its value
+    if key in a_dictionary:
+        a_dictionary[key] = value
+    # If the key doesn't exist in the dictionary, add it with the given value
+    else:
+        a_dictionary[key] = value
+    
+    return a_dictionary
