3ce1d98d9bb9221b350234a8b57c6f090ba287f3
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-07-25 11:36:32 +0300

    0-square_matrix_simple.py

---
+++ python-data_structures/0-square_matrix_simple.py
@@ -0,0 +1,10 @@
+def square_matrix_simple(matrix=[]):
+    # Create a new matrix with the same size as the input matrix
+    result = [[0 for j in range(len(matrix[i]))] for i in range(len(matrix))]
+    
+    # Fill in the new matrix with the square values of the input matrix
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            result[i][j] = matrix[i][j] ** 2
+    
+    return result
