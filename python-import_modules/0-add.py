d6c2869540252a84fd2067d698c98898d766722f
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-07-25 11:15:58 +0300

    0-add.py

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
-if __name__ == "__main__":
+if __name__ == '__main__':
+    a = 1
+    b = 2
     from add_0 import add
-
-a = 1
-b = 2
-print('{} + {} = {}'.format(a, b, add(a, b)))
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
