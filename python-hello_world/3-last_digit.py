e0e7c1aa227b98542059fe3f95859766cf309ac5
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-07-27 14:33:41 +0300

    Update 3-last_digit.py

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,16 +1,19 @@
 import random
 
-# Assign a random signed number to the variable number
-number = random.randint(-10000, 10000)
+try:
+    # Assign a random signed number to the variable number
+    number = random.randint(-10000, 10000)
 
-# Get the last digit of number using modulo operator %
-last_digit = abs(number) % 10
+    # Get the last digit of number using modulo operator %
+    last_digit = abs(number) % 10
 
-# Print the last digit of number and additional information based on its value
-print("Last digit of", number, "is", last_digit, end=" ")
-if last_digit > 5:
-    print("and is greater than 5")
-elif last_digit == 0:
-    print("and is 0")
-else:
-    print("and is less than 6 and not 0")
+    # Print the last digit of number and additional information based on its value
+    print(f"Last digit of {number} is {last_digit}", end=" ")
+    if last_digit > 5:
+        print("and is greater than 5")
+    elif last_digit == 0:
+        print("and is 0")
+    else:
+        print("and is less than 6 and not 0")
+except TypeError:
+    print("Error: Input must be an integer.")
