8c44813ecd06857c2dfea622df9152ba6dac2564
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-07-27 14:53:02 +0300

    3-last_digit.py

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,5 +1,7 @@
+#!/usr/bin/python3
 import random
-
+number = random.randint(-10000, 10000)
+# YOUR CODE HERE
 try:
     # Assign a random signed number to the variable number
     number = random.randint(1, 10000)
