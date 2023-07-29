ffb197f9a226eadeb9033bf7ff95a36260169ce5
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-07-29 11:03:30 +0300

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,27 +1,3 @@
 class Square:
     def __init__(self, size):
         self.__size = size
-mysquare = Square(3)
-print(type(mysquare))
-print(mysquare.__dict__)
-
-mysquare = Square(89)
-print(type(mysquare))
-print(mysquare.__dict__)
-
-mysquare = Square(3)
-print(type(mysquare))
-print(mysquare.__dict__)
-
-try:
-    print(mysquare.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(mysquare._Square__size)
-except Exception as e:
-    print(e)
-mysquare = Square(3)
-print(type(mysquare))
-print(mysquare.__dict__)
