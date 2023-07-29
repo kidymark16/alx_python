92d388904bde50843c406f0531bbbb31767ab038
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-07-29 10:59:41 +0300

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -25,10 +25,3 @@ except Exception as e:
 mysquare = Square(3)
 print(type(mysquare))
 print(mysquare.__dict__)
-
-#!/usr/bin/python3
-Square = __import__('0-square').Square
-
-my_square = Square(3)
-print(type(my_square))
-print(my_square.__dict__)
