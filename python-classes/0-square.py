d619142de66d5dd8e9379d07420af8545ae8083b
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-07-31 11:41:55 +0300

    Done

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Class Square definition"""
+# 0-square.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Define a class Square."""
 
 
 class Square:
-    """an empty class called Square that defines a square"""
+    """Represent a square."""
     pass
