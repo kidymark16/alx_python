34355b8308531e095898e42f95e610910c4338d3
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-08-02 11:55:17 +0300

    Update 2-main.py

--- python-classes/tests/2-main.py
+++ python-classes/tests/2-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 Square = __import__('2-square').Square
-
+mysquare = Square() print(type(mysquare)) print(mysquare.dict_)
 my_square_1 = Square(3)
 print(type(my_square_1))
 print(my_square_1.__dict__)
