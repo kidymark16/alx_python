6f5bf020712948d54c92afd3fc9f3f5e2ff3c958
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-08-02 11:34:06 +0300

    try 4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,30 +1,55 @@
 #!/usr/bin/python3
+"""Definition of Square class"""
+
 
 class Square:
+    """Defines a class square.
+
+    Attributes:
+        __size: Size of the square
+
+    Args:
+        size: Size of the square
+
+    Raises:
+        TypeError: If size is not an integer
+        ValueError: If size is less than 0
+    """
     def __init__(self, size=0):
-        self.size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """Calculates the area of the square.
+
+        Returns:
+            The area of the square
+        """
+        return self.__size ** 2
 
     @property
     def size(self):
+        """Gets the size of the square.
+
+        Returns:
+            The size the square
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
+        """Sets the size.
+
+        Args:
+            value: Value to set size
+        """
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
-
-    def area(self):
-        return self.__size ** 2
-
-    def my_print(self):
-        if self.__size == 0:
-            print()
-        else:
-            for i in range(self.__size):
-                for j in range(self.__size):
-                    print("#", end="")
-                print()
