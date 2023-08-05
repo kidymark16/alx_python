a52f749d20867f9c019a33a9616c3665bd626de6
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-08-05 11:25:26 +0300

    Create 6-my_github.py

---
+++ python-network_1/6-my_github.py
@@ -0,0 +1,16 @@
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]  # GitHub username
+    password = sys.argv[2]  # Personal access token
+
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(username, password))
+
+    if response.status_code == 200:
+        user_data = response.json()
+        user_id = user_data.get("id")
+        print(user_id)
+    else:
+        print("None")
