3cbb7116a3065a4a56440e8e0ad2514c07ba708e
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-08-05 11:22:45 +0300

    1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,12 +1,23 @@
-
 #!/usr/bin/python3
 import requests
 import sys
 
 if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/auth/sign_in"
+    url = sys.argv[1]  # Get the URL from the command line argument
     response = requests.get(url)
-    content = response.text
 
-    print(content)
+    # Case: http://0.0.0.0:5050 with X-Request-Id="School"
+    if response.headers.get("X-Request-Id") == "School":
+        print("School")
 
+    # Case: http://0.0.0.0:5050 with X-Request-Id=98 and one redirection
+    elif response.history and response.headers.get("X-Request-Id") == "98":
+        print(response.headers.get("X-Request-Id"))
+
+    # Case: http://0.0.0.0:5050 without X-Request-Id in the HTTP header
+    elif not response.headers.get("X-Request-Id"):
+        print("No X-Request-Id header")
+
+    # Default case: Print the value of X-Request-Id
+    else:
+        print(response.headers.get("X-Request-Id"))
