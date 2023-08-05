db8472b2b3036ea2bdddd4fabf0749a3a2e498b3
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-08-05 11:17:01 +0300

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-import requests
-import sys
+import request
 
-if __name__ == "__main__":
-    url = sys.argv[1]  # Get the URL from the command line argument
-    response = requests.get(url)
-    request_id = response.headers.get('X-Request-Id')  # Retrieve the value of the X-Request-Id header
-
-    print(request_id)
+with request.urlopen("https://intranet.hbtn.io/status") as response:
+    r = response.read()
+    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
+          .format(type(r), r, r.decode('utf-8')))
