d4eada0d52d1f544ba9e7b588c0b5edad8c5de28
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-08-05 11:58:19 +0300

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,9 +5,6 @@ if __name__ == "__main__":
     url = "https://alu-intranet.hbtn.io/status"
     response = requests.get(url)
 
-    if response.status_code == 200:
-        response_body = response.json()
-        for key, value in response_body.items():
-            print(f"- {key}: {value}")
-    else:
-        print(f"Error: {response.status_code}")
+    print("Body response:")
+    print(f"\t- type: {type(response.text)}")
+    print(f"\t- content: {response.text}")
