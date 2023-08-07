83d585737730c9cc28a55fcc57bf525ca247834f
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-08-07 09:55:40 +0300

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,4 +1,12 @@
-#!/usr/bin/env python3
+#!/bin/bash
+kill -9 `cat web.pid` > /dev/null 2>&1
+sleep 2
+
+python3 "$1" > /dev/null 2>&1 &
+echo $! > web.pid
+
+sleep 5
+
 """
 Fetches the status from https://alu-intranet.hbtn.io/status
 """
@@ -27,4 +35,3 @@ if __name__ == "__main__":
     print("Body response:")
     print("\t- type:", type(body))
     print("\t- content:", body)
-    
