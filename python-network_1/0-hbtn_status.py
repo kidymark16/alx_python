d4c4c99cb43b238dd4f0f3245a7117c7319a14c0
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-08-07 09:57:24 +0300

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,13 +1,4 @@
 #!/usr/bin/env python3
-
-kill -9 `cat web.pid` > /dev/null 2>&1
-sleep 2
-
-python3 "$1" > /dev/null 2>&1 &
-echo $! > web.pid
-
-sleep 5
-
 """
 Fetches the status from https://alu-intranet.hbtn.io/status
 """
