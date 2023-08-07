70703151d1034399657206953bf56e36983da614
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-08-07 10:04:38 +0300

    1-hbtn_header.py

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,23 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+Sends a request to a URL and displays the value of the X-Request-Id header in the response.
+"""
+
 import requests
 import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]  # Get the URL from the command line argument
+    url = sys.argv[1]
     response = requests.get(url)
-
-    # Case: http://0.0.0.0:5050 with X-Request-Id="School"
-    if response.headers.get("X-Request-Id") == "School":
-        print("School")
-
-    # Case: http://0.0.0.0:5050 with X-Request-Id=98 and one redirection
-    elif response.history and response.headers.get("X-Request-Id") == "98":
-        print(response.headers.get("X-Request-Id"))
-
-    # Case: http://0.0.0.0:5050 without X-Request-Id in the HTTP header
-    elif not response.headers.get("X-Request-Id"):
-        print("No X-Request-Id header")
-
-    # Default case: Print the value of X-Request-Id
-    else:
-        print(response.headers.get("X-Request-Id"))
+    x_request_id = response.headers.get("X-Request-Id")
+    print(x_request_id)
