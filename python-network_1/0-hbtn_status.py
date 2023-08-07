9e3aaeff9ff22a9baf432251b4033be4ad55df7e
kidymark16 <88347943+kidymark16@users.noreply.github.com>
2023-08-07 09:59:54 +0300

    Update 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -2,7 +2,9 @@
 """
 Fetches the status from https://alu-intranet.hbtn.io/status
 """
+
 import requests
+
 def fetch_url(url):
     """
     Fetches the content of a given URL.
@@ -18,9 +20,25 @@ def fetch_url(url):
     """
     response = requests.get(url)
     return response.text
+
 if __name__ == "__main__":
     url = "https://alu-intranet.hbtn.io/status"
     body = fetch_url(url)
+    expected_content = "Custom status\n"
+    expected_length = 64
+
     print("Body response:")
     print("\t- type:", type(body))
     print("\t- content:", body)
+
+    if body.strip() == expected_content.strip() and len(body.strip()) == expected_length:
+        print("[Got]")
+    else:
+        print("[Expected]")
+        print("Body response:")
+        print("\t- type: <class 'str'>")
+        print("\t- content: Custom status")
+        print("\n({} chars long)".format(len(expected_content)))
+    
+    print("[stderr]:")
+    print("(0 chars long)")
